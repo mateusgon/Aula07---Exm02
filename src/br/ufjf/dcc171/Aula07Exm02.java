53bc7644a30b5c3759df00415d88dab72e2fa130
ice <ice@lab06m27.nrc.ice.ufjf.br>
2017-08-31 21:53:49 -0300

    Terminado

---
+++ src/br/ufjf/dcc171/Aula07Exm02.java
@@ -0,0 +1,15 @@
+package br.ufjf.dcc171;
+
+import javax.swing.JFrame;
+
+public class Aula07Exm02 {
+
+    public static void main(String[] args) {
+        JanelaListaPessoa janela = new JanelaListaPessoa();
+        janela.setSize(400, 200);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setLocationRelativeTo(null);
+        janela.setVisible(true);
+    }
+    
+}
---
+++ src/br/ufjf/dcc171/JanelaListaPessoa.java
@@ -0,0 +1,90 @@
+package br.ufjf.dcc171;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+public class JanelaListaPessoa extends JFrame {
+
+        private final JLabel lblNome = new JLabel("Nome");
+        private final JLabel lblIdade = new JLabel("Idade");
+        private final JTextField txtNome = new JTextField(10);
+        private final JTextField txtIdade = new JTextField(10);
+        private final JButton btnExcluir = new JButton("Excluir");
+        private final DefaultListModel modelo = new DefaultListModel();
+        private final JList lstLista = new JList(modelo);
+        
+    public JanelaListaPessoa() throws HeadlessException {
+        super("Lista");
+        setLayout(new BorderLayout(5,5));
+        JPanel pnlNome = new JPanel();
+        pnlNome.setLayout(new BorderLayout(5,5));
+        pnlNome.add(lblNome, BorderLayout.WEST);
+        pnlNome.add(txtNome, BorderLayout.CENTER);
+        JPanel pnlIdade = new JPanel();
+        pnlIdade.setLayout(new BorderLayout(5,5));
+        pnlIdade.add(lblIdade, BorderLayout.WEST);
+        pnlIdade.add(txtIdade, BorderLayout.CENTER);
+        JPanel pnlEntrada = new JPanel(new FlowLayout());
+        pnlEntrada.add(pnlNome);
+        pnlEntrada.add(pnlIdade);
+        add(pnlEntrada, BorderLayout.NORTH);
+        add(btnExcluir, BorderLayout.SOUTH);
+        add(new JScrollPane(lstLista), BorderLayout.CENTER);
+        
+        
+        for (int i = 0; i<3; i++)
+        {
+            Pessoa p = new Pessoa();
+            p.setNome("Pessoa" + i);
+            p.setIdade(18+i);
+            modelo.addElement(p);
+        }
+        
+        txtNome.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (txtNome.getText().isEmpty()){
+                    txtIdade.requestFocus();
+                    return;
+                }
+            }
+        });
+        
+        txtIdade.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Integer idade = Integer.parseInt(txtIdade.getText());
+                String nome = txtNome.getText();
+                Pessoa p = new Pessoa();
+                p.setNome(nome);
+                p.setIdade(idade);
+                modelo.addElement(p);
+            }
+        });
+        
+        btnExcluir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                List selecionados = lstLista.getSelectedValuesList();
+                for (Object selecionado: lstLista.getSelectedValuesList())
+                {
+                    System.out.println(selecionado);
+                    modelo.removeElement(selecionado);
+                }
+            }
+        });
+    }
+    
+}
